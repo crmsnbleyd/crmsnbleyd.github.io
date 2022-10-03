d78ed07ad85056ef49a857b72b243ef45d6cb6d1
Andrew <arnav.jose@gmail.com>
2022-10-03 23:43:52 +0530

    pelican

---
+++ electra/pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'Arnav Andrew Jose'
+SITENAME = 'Electra'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/crmsnbleyd'),
+          ('Linkedin', 'https://www.linkedin.com/in/arnav-andrew-jose'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
